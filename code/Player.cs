635a13587459ce0148d1b24ca049987295a2e3f2
Roddie Kieley <rkieley@unifiedsoftworx.com>
2020-01-21 00:06:50 -0330

    Updated player for animated sprites.

--- code/Player.cs
+++ code/Player.cs
@@ -5,42 +5,61 @@ using System;
 public class Player : KinematicBody2D
 {
 	private const float movementAmount = 100.0f;
+	private const float jumpAmount = 400.0f;
 	
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 	private Vector2 linearVelocity;
-	//private KinematicBody2D player;
+	private Vector2 floorNormal = new Vector2(0.0f, -1.0f);
+	private float gravity = 10.0f;
+	private Sprite sprite;
+	private KinematicBody2D player;
+	private AnimatedSprite playerSprite;
 
 	// Called when the node enters the scene tree for the first time.
-	//public override void _Ready() {}
+	public override void _Ready()
+	{
+		SceneTree tree = GetTree();
+		Node currentScene = tree.CurrentScene;
+		Node rootNode = tree.GetRoot();
+		player = currentScene.GetNode("/root/World/Player") as KinematicBody2D;
+		playerSprite = currentScene.GetNode("/root/World/Player/PlayerSprite") as AnimatedSprite;
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	//public override void _Process(float delta) {}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		//player = GetNode("player") as KinematicBody2D;
-		
-		linearVelocity = Vector2.Zero;
+		linearVelocity.x = 0.0f;
+		linearVelocity.y += gravity;
 		if (Input.IsActionPressed("ui_right"))
 		{
 			linearVelocity.x = movementAmount;
+			playerSprite.FlipH = false;
+			playerSprite.Play("run");
 		}
 		else if (Input.IsActionPressed("ui_left"))
 		{
 			linearVelocity.x = -movementAmount;
+			playerSprite.FlipH = true;
+			playerSprite.Play("run");
+		}
+		else
+		{
+			playerSprite.Play("idle");
 		}
 		
-		if (Input.IsActionPressed("ui_up"))
+		if (IsOnFloor())
 		{
-			linearVelocity.y = -movementAmount;
+			if (Input.IsActionJustPressed("ui_up"))
+			{
+				linearVelocity.y = -jumpAmount;
+			}
 		}
-		else if (Input.IsActionPressed("ui_down"))
+		else
 		{
-			linearVelocity.y = movementAmount;
+			playerSprite.Play("jump");
 		}
 		
-		MoveAndSlide(linearVelocity);
+		linearVelocity = MoveAndSlide(linearVelocity, floorNormal);
 	}
 }
