d4d777f79759782ee0ebcc102802a3aedef7f41b
Ricardo Luders <rluders@redhat.com>
2020-01-23 20:14:29 -0300

    Remove the piece when it exit the screen

--- src/LevelController.cs
+++ src/LevelController.cs
@@ -5,11 +5,13 @@ public class LevelController : Node2D
 {
     [Export((PropertyHint) 24, "17/17:PackedScene")]
     public Godot.Collections.Array<PackedScene> Scenes;
+    private Godot.Collections.Array<Node> InstancedScenes;
 
     private Node2D LevelHolder;
 
     public override void _Ready()
     {
+        InstancedScenes = new Godot.Collections.Array<Node>();
         SpawnRandomScene();
     }
 
@@ -22,11 +24,15 @@ public class LevelController : Node2D
         Node sceneInstance = scene.Instance();
         sceneInstance.GetNode("VisibilityNotifier2D").Connect("screen_exited", this, "_OnPieceScreenExited");
 
+        InstancedScenes.Add(sceneInstance);
         AddChild(sceneInstance);
     }
 
     public void _OnPieceScreenExited()
     {
+        // @FIXME Probably not the best solution
+        InstancedScenes[0].QueueFree();
+        InstancedScenes.RemoveAt(0);
         GD.Print("Piece isn't visible anymore. Destroy it.");
     }
 }
