65e6bdc85c51a85f6d5a21321b7c15eb3c0ee78d
Ernesto González <100318050@alumnos.uc3m.es>
2020-02-18 22:15:25 +0100

    Added Dash class

--- src/Actors/Player/Player.cs
+++ src/Actors/Player/Player.cs
@@ -3,30 +3,30 @@ using System;
 
 public class Player : Actor
 {
-    public StateMachine StateMachine;
+	public StateMachine StateMachine;
 
-    public AnimationPlayer AnimationPlayer;
+	public AnimationPlayer AnimationPlayer;
 
-    public Vector2 LookDirection = new Vector2(1, 1);
+	public Vector2 LookDirection = new Vector2(1, 1);
 
-    public override void _Ready()
-    {
-        this.StateMachine = this.GetNode("StateMachine") as StateMachine;
-        this.AnimationPlayer = this.GetNode("AnimationPlayer") as AnimationPlayer;
-    }
+	public override void _Ready()
+	{
+		this.StateMachine = this.GetNode("StateMachine") as StateMachine;
+		this.AnimationPlayer = this.GetNode("AnimationPlayer") as AnimationPlayer;
+	}
 
-    public virtual void Flip(Vector2 direction)
-    {
-        if (direction.x != 0 && this.LookDirection != direction) {
-            Position2D bodyPivot = this.GetNode("BodyPivot") as Position2D;
+	public virtual void Flip(Vector2 direction)
+	{
+		if (direction.x != 0 && this.LookDirection != direction) {
+			Position2D bodyPivot = this.GetNode("BodyPivot") as Position2D;
 
-            this.LookDirection = direction;
+			this.LookDirection = direction;
 
-            bodyPivot.Scale = new Vector2(
-                direction.x,
-                1
-            );
-        }
-    }
+			bodyPivot.Scale = new Vector2(
+				direction.x,
+				1
+			);
+		}
+	}
 
 }
