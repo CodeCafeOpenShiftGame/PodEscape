ceebd178a67bfd82a109ee662b0ac013cdb5e5c1
dudash <jason.dudash@gmail.com>
2020-02-25 23:22:47 -0500

    hide quit button in HTML5 mode - obviously needs prettied up a bit by the art team.

--- src/Scenes/Main.cs
+++ src/Scenes/Main.cs
@@ -5,12 +5,21 @@ public class Main : Control
 {
 	private Label titleLabel;
 	private GameManager gameManager;
+	private Button quitButton;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		titleLabel = GetNode<Label>("Title");
 		gameManager = GetNode<GameManager>("/root/GameManager");
+		quitButton = GetNode<Button>("VBoxContainer/QuitButton");
+		
+		if (OS.GetName() == "HTML5")
+		{
+			// no quitting in HTML web browser mode
+			quitButton.Visible = false;
+		}
+		
 	}
 	
 	private void _on_PlayButton_button_up()
