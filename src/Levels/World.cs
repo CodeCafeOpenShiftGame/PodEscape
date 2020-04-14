f59c3c321eeb7b022a7f1cbd392f00e7d8292437
Ricardo Luders <rluders@redhat.com>
2020-04-14 08:08:17 +0200

    Change audios from wav to ogg

--- src/Levels/World.cs
+++ src/Levels/World.cs
@@ -4,17 +4,17 @@ using System;
 public class World : Node
 {
 
-    private AudioStreamPlayer MusicStreamPlayer;
+	private AudioStreamPlayer MusicStreamPlayer;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        this.MusicStreamPlayer = this.GetNode<AudioStreamPlayer>("MusicStreamPlayer");
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.MusicStreamPlayer = this.GetNode<AudioStreamPlayer>("MusicStreamPlayer");
+	}
 
  // Called every frame. 'delta' is the elapsed time since the previous frame.
  public override void _Process(float delta)
  {
-     if (GameManager.AudioOn) MusicStreamPlayer.StreamPaused = false; else MusicStreamPlayer.StreamPaused = true;
+	 if (GameManager.AudioOn) MusicStreamPlayer.StreamPaused = false; else MusicStreamPlayer.StreamPaused = true;
  }
 }
