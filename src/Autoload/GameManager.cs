94778c186a82fed125c95bc6703b910e7f04c781
Ricardo Lüders <ricardo@luders.com.br>
2020-04-17 21:25:28 +0200

    Adds collectable new sprites, animations and events

--- src/Autoload/GameManager.cs
+++ src/Autoload/GameManager.cs
@@ -3,51 +3,57 @@ using System;
 
 public class GameManager : Node
 {
-	[Signal]
-	delegate void GameManagerReady();
     [Signal]
-	delegate void UpdatedScore(int score);
+    delegate void GameManagerReady();
+    [Signal]
+    delegate void UpdatedScore(int score);
     [Signal]
     delegate void UpdatedGracePeriod(int gracePeriod);
     [Signal]
     delegate void GracePeriodExpired();
 
-	public int Score { get; private set; } = 0;
+    public int Score { get; private set; } = 0;
     public int GracePeriod { get; set; } = 0;
-	private Timer everySecond = new Timer();
+    private Timer everySecond = new Timer();
 
-	public static Boolean AudioOn = true;
+    public static Boolean AudioOn = true;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		EmitSignal(nameof(GameManagerReady)); // just testing the C# signals
-		this.AddChild(everySecond);
-		everySecond.Connect("timeout", this, nameof(_on_Timer_timeout));
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        EmitSignal(nameof(GameManagerReady)); // just testing the C# signals
+        this.AddChild(everySecond);
+        everySecond.Connect("timeout", this, nameof(_on_Timer_timeout));
         //this.Connect("PlayerDied", Player, "_on_PlayerDied");
     }
 
-	public void newGame()
-	{
-		this.Score = 0;
-		GetTree().ChangeScene("res://src/Levels/World.tscn");
-		GetTree().Paused = false;
+    public void newGame()
+    {
+        this.Score = 0;
+        GetTree().ChangeScene("res://src/Levels/World.tscn");
+        GetTree().Paused = false;
         everySecond.Start(1);
-	}
+    }
 
-	public void endGame()
-	{
-		//GD.Print("player decided to end the current game");
+    public void endGame()
+    {
+        //GD.Print("player decided to end the current game");
         GD.Print("GameManager::endGame()");
-		// TODO: anything else - do they get a high score if they quit?
+        // TODO: anything else - do they get a high score if they quit?
         this.everySecond.Stop();
-		//GetTree().ChangeScene("res://src/Scenes/Main.tscn");
-	}
+        //GetTree().ChangeScene("res://src/Scenes/Main.tscn");
+    }
 
-	private void _on_Timer_timeout()
-	{
-		this.Score = this.Score + 10;  // TODO: this is just for demo, need to calculate based on dist traveled not just time
-		EmitSignal(nameof(UpdatedScore), this.Score);
+    private void _OnItemCollected(Collectable item)
+    {
+        this.Score += item.ScorePoints;
+        EmitSignal(nameof(UpdatedScore), this.Score);
+    }
+
+    private void _on_Timer_timeout()
+    {
+        this.Score = this.Score + 10;  // TODO: this is just for demo, need to calculate based on dist traveled not just time
+        EmitSignal(nameof(UpdatedScore), this.Score);
 
         this.GracePeriod = this.GracePeriod - 1; // TODO: GracePeriod elapsing by variable due to game events?
         EmitSignal(nameof(UpdatedGracePeriod), this.GracePeriod);
--- src/Collectable/Collectable.cs
+++ src/Collectable/Collectable.cs
@@ -3,18 +3,26 @@ using System;
 
 public class Collectable : Node2D
 {
+    [Signal]
+    delegate void ItemCollected(Collectable item);
+
     [Export]
     public int ScorePoints = 10;
 
     protected AnimationPlayer animationPlayer;
+    private GameManager gameManager;
 
     public override void _Ready()
     {
+        this.gameManager = GetNode<GameManager>("/root/GameManager");
         this.animationPlayer = this.GetNode<AnimationPlayer>("Area2D/AnimationPlayer");
+
+        this.Connect(nameof(ItemCollected), this.gameManager, "_OnItemCollected");
     }
 
-    public virtual void _OnBodyEntered(PhysicsBody2D body)
+    public void _OnBodyEntered(PhysicsBody2D body)
     {
+        EmitSignal(nameof(ItemCollected), this);
         this.animationPlayer.Play("fadeOut");
     }
 }
