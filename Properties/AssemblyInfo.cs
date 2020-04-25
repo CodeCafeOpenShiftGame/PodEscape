feb27484d22e63beafa041686d97d432fa65a93e
Roddie Kieley <rkieley@unifiedsoftworx.com>
2020-04-25 16:30:28 -0230

    Music now on by default. AssemblyInfo updated.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("PodEscape")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Play a kubernetes pod trying to escape from forcible shutdown during the grace period!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -16,7 +16,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.5.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
